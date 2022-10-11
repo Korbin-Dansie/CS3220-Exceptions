b3a5a81d39651f2bf688e28fac11d96047c86312
Korbin Dansie <dansiekm@gmail.com>
2022-10-10 19:26:04 -0600

    Revert "added files"
    This reverts commit e1c08095313c68ee8687996f834fb10c411fd03b.

--- src/main/java/WrongFileException.java
+++
@@ -1,7 +0,0 @@
-// Create a custom exception
-public class WrongFileException extends Exception {
-    @Override
-    public String getMessage() {
-        return "You choose the wrong file!";
-    }
-}
